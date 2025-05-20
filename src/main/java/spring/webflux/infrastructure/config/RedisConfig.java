d80bd3ee6e4e16032b28b6f5389a1073b885f304
inkyu192 <inkyu192@gmail.com>
2025-05-20 21:26:18 +0900

    redis 설정 추가

--- src/main/java/spring/webflux/infrastructure/config/RedisConfig.java
+++ src/main/java/spring/webflux/infrastructure/config/RedisConfig.java
@@ -2,6 +2,7 @@ package spring.webflux.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -11,26 +12,27 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import lombok.RequiredArgsConstructor;
 import spring.webflux.infrastructure.properties.RedisProperties;
 
-// @RequiredArgsConstructor
-// @Configuration(proxyBeanMethods = false)
-// public class RedisConfig {
-//
-// 	private final RedisProperties redisProperties;
-//
-// 	@Bean
-// 	public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
-// 		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
-// 	}
-//
-// 	@Bean
-// 	public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
-// 		ReactiveRedisConnectionFactory connectionFactory
-// 	) {
-// 		RedisSerializationContext<String, String> context = RedisSerializationContext
-// 			.<String, String>newSerializationContext(RedisSerializer.string())
-// 			.value(RedisSerializer.string())
-// 			.build();
-//
-// 		return new ReactiveRedisTemplate<>(connectionFactory, context);
-// 	}
-// }
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+public class RedisConfig {
+
+	private final RedisProperties redisProperties;
+
+	@Bean
+	@Primary
+	public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+	}
+
+	@Bean
+	public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
+		ReactiveRedisConnectionFactory connectionFactory
+	) {
+		RedisSerializationContext<String, String> context = RedisSerializationContext
+			.<String, String>newSerializationContext(RedisSerializer.string())
+			.value(RedisSerializer.string())
+			.build();
+
+		return new ReactiveRedisTemplate<>(connectionFactory, context);
+	}
+}
